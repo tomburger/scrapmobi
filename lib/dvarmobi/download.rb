efa113c55cd93c3c793fbf5468c401aaabb9c8ea
Tom Burger <tomas@burger.cz>
2013-01-08 22:47:03 +0100

    Introducing DSL for download process

--- lib/dvarmobi/download.rb
+++ lib/dvarmobi/download.rb
@@ -1,45 +1,63 @@
 require 'open-uri'
 require 'nokogiri'
 require 'cgi'
+class Downloader
+  def initialize
+    @host = ''
+    @url = ''
+    @html = nil
+  end
+  def set_host(host)
+    @host = host
+    return self
+  end
+  def start_with(url)
+    @url = @host+url
+    return self
+  end
+  def get_page
+    @html = Nokogiri::HTML(open(@url))
+    @url = ''
+    return self
+  end
+  def get_link(selector)
+    link = @html.at_css(selector)
+    @url = link['href']
+    @url=@host+@url if (@url[0,4].downcase!='http')
+    @html = nil
+    return self
+  end
+  def get_content(selector)
+    @html = @html.css(selector)
+    return self
+  end
+  def remove(selector)
+    @html.css(selector).remove
+    return self
+  end
+  def to_text
+    txt = @html.to_html
+    txt = txt.encode("UTF-8")
+    txt = CGI.unescapeHTML(txt)
+    return txt 
+  end
+end
 
 class Download
   def self.download(args)
     args.each do |page|
-      content = download_one(page)
-      content = convert(page, content)
-      yield page, content
+      pg_data = ScrapData.config.get(page)
+      content = Downloader.new
+      content.set_host(pg_data.host)
+             .start_with(pg_data.index)
+             .get_page
+             .get_link(pg_data.ix_sel)
+             .get_page
+             .get_content(pg_data.tx_sel)
+      pg_data.remove.each do |selector|
+        content.remove(selector)
+      end
+      yield page, content.to_text
     end
-  end
-  def self.download_one(page)
-    pg_data = ScrapData.config.get(page)
-    
-    # get index page, which is always the same, find the link to current page
-    index = Nokogiri::HTML(open(pg_data.host+pg_data.index))
-    link = index.at_css(pg_data.ix_sel)
-    tx_url = link[pg_data.attr]
-    
-    # prepend relative URL with host 
-    if (pg_data.host!=tx_url[0,pg_data.host.length]) 
-      tx_url = pg_data.host+tx_url 
-    end
-    
-    # get actual page with content
-    tx_page = Nokogiri::HTML(open(tx_url))
-    
-    return tx_page.at_css(pg_data.tx_sel)
-  end 
-  def self.convert(page, text)
-    pg_data = ScrapData.config.get(page)
-    
-    # remove not wanted pieces
-    pg_data.remove.each do |selector|
-      text.css(selector).remove()
-    end
-    
-    txt = text.to_html()
-    txt = txt.encode("UTF-8")
-    txt = CGI.unescapeHTML(txt)
-    
-    return txt 
   end
 end
